4165010bd46c0b365be7f6f47bc05e7342718c47
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2019-05-26 12:14:42 -0400

    add view source link

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ release = ""
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinxcontrib.fulltoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinxcontrib.fulltoc", "sphinx.ext.viewcode"]
 
 autodoc_member_order = "bysource"
 intersphinx_mapping = {
